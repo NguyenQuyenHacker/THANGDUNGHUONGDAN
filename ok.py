3eaac69fbe70f34f7f0f7b80addfcf0350da06c8
Quyennkc <anhlandibo862@gmail.com>
2025-09-21 13:57:45 +0700

    first commit

--- ok.py
+++ ok.py
@@ -1,1 +1,12 @@
-print(1+2)
+import sqlite3
+
+conn = sqlite3.connect("nhahang.db")
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM Nhân_sự")
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+conn.close()
